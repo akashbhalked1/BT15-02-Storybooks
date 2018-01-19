7d5684adfc2dcdf3515c54be3aa732cfb270f633
Chi Lin <chilin89117@gmail.com>
2018-01-18 21:56:42 -0800

    Up to lecture 52

--- routes/stories.js
+++ routes/stories.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const {ensureAuth, ensureGuest} = require('../helpers/auth');
 
 router.get('/', (req, res) => {
   res.render('stories/index');
 });
 
-router.get('/add', (req, res) => {
+router.get('/add', ensureAuth, (req, res) => {
   res.render('stories/add');
 });
 
